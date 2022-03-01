924d34b0988de8509c6920c201531ce7b0424f03
ccllaarraa <clarapasteau@gmail.com>
2022-03-01 18:35:38 +0100

    mobile

--- front-page.php
+++ front-page.php
@@ -2,38 +2,92 @@
 get_header();
 ?>
 
+<div class="front-desktop">
+    <a href="./about"><img class="about" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/about.svg" alt="about"></a>
+    <div class="projects">
+    <?php 
 
-<div class="projects">
-<?php 
+    $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 100 ) ); 
+    while ( $loop->have_posts() ) : $loop->the_post();
 
-$loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 100 ) ); 
-while ( $loop->have_posts() ) : $loop->the_post();
-
-?>
-
-
-<?php 
-$images = get_field('gallery');
-if( $images ): ?>
-    <ul class="project">
-        <?php foreach( $images as $image ): ?>
-            
-            <li>
-                <a  class="pan" href="#" data-big="<?php echo $image['url']; ?>" alt="<?php echo esc_attr($image['alt']); ?>" >
-                     <img class="thumb-img" src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                </a>
-              
-                <p><?php the_title(); ?><?php echo esc_html($image['caption']); ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
+    ?>
 
 
+    <?php 
+    $images = get_field('gallery');
+    if( $images ): ?>
+        <ul class="project">
+            <?php foreach( $images as $image ): ?>
+                
+                <li>
+                    <a  class="pan" href="#" data-big="<?php echo $image['url']; ?>" alt="<?php echo esc_attr($image['alt']); ?>" >
+                        <img class="thumb-img" src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                    </a>
+                
+                    <p class="caption"><?php the_title(); ?> 
+                    <?php  if($image['caption']) : ?>
+                        ~
+                    <?php echo esc_html($image['caption']); ?>
+                    <?php endif; ?> </p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 
 
 
-<?php endwhile; ?>
+
+
+    <?php endwhile; ?>
+    </div>
+
+</div>
+
+<!-- mobile template -->
+ 
+
+
+<div class="front-mobile">
+    <a href="./about"><img class="about" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/about.svg" alt="about"></a>
+        <div class="projects">
+        <?php 
+
+        $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 100 ) ); 
+        while ( $loop->have_posts() ) : $loop->the_post();
+
+        ?>
+
+
+        <?php 
+        $images = get_field('gallery');
+        if( $images ): ?>
+            <ul class="project">
+                <?php foreach( $images as $image ): ?>
+                    
+                    <li>
+                      
+                            <img class="thumb-img" src="<?php echo $image['url']; ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                      
+                    
+                        <p class="caption"><?php the_title(); ?> 
+                        <?php  if($image['caption']) : ?>
+                            ~
+                        <?php echo esc_html($image['caption']); ?>
+                        <?php endif; ?> </p>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
+
+
+
+
+    <?php endwhile; ?>
+    </div>
+
+
+
 </div>
 
 
--- page-about.php
+++ page-about.php
@@ -6,49 +6,20 @@
 get_header();
 ?>
 
+<a class="about about-back"  onclick="history.back()">back</a>
+
 <div class="about-page">
-    <a class="about about-back"  onclick="history.back()">back</a>
+   
 
     <?php 
-        $bio = get_field('bio'); 
+        $col1 = get_field('col1'); 
+        $col2 = get_field('col2'); 
+        $col3 = get_field('col3'); 
     ?>
-    <div class='about-desc'><?php echo $bio; ?></div>
-    <div class="about-liens">
-    <?php if( have_rows('liens') ): ?>
-         <?php while ( have_rows('liens') ) : the_row(); ?>
-           <?php $link= get_sub_field('about_lien'); ?>
-                <?php if( $link ) :
-                                $link_url = $link['url'];
-                                $link_title = $link['title'];
-                                $link_target = $link['target'] ? $link['target'] : '_self';
-                                 ?>
-                                <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-            <?php endif ?>
-         <?php endwhile; ?>
-    <?php endif; ?>
-    </div>
-    <div class="friends-liens">
-        <span>Visit my friends websites</span>
-        <div class="friends-list"> 
-        <?php if( have_rows('friends_lien') ): ?>
-            <?php while ( have_rows('friends_lien') ) : the_row(); ?>
-            <?php $link= get_sub_field('lien_friend'); ?>
-                    <?php if( $link ) :
-                                    $link_url = $link['url'];
-                                    $link_title = $link['title'];
-                                    $link_target = $link['target'] ? $link['target'] : '_self';
-                                    ?>
-                                    <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-                <?php endif ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
-        </div>
-    </div>
 
-
-
-</div>
-
+    <div class="col col1"><?php echo $col1; ?></div>
+    <div class="col col2"><?php echo $col2; ?></div>
+    <div class="col col3"><?php echo $col3; ?></div>
 
 <?php
 
